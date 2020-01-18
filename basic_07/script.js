26b3bf26d61ad7409df01dd856b3f22e6f4d19f0
Leonid-droid <lealro2011@gmail.com>
2020-01-18 17:27:11 +0300

    Окончание урока Функции

--- basic_07/script.js
+++ basic_07/script.js
@@ -1,3 +1,37 @@
-function showFirstMassage(text) {
-    alert(text);
+// let num = 20;
+// function showFirstMassage(text) {
+//     alert(text);
+//     let num = 10;
+//     console.log(num);    
+// }
+// showFirstMassage('Hello!!!');
+// console.log(num);
+
+function calc(a, b) {
+    return (a + b);
 }
+console.log(calc(12, 56));
+console.log(calc(34, 99));
+
+function retVar() {
+    let num = 50;
+    return num;
+}
+let another = retVar();
+console.log(another);
+
+let calcul = function (a, b) {
+    return (a + b);
+}
+console.log(calcul(22, 44));
+
+let str = 'text';
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+let twelve = '12.2';
+console.log(Math.round(twelve));
+
+let twelves = '12.2px';
+console.log(parseInt(twelves));
+console.log(parseFloat(twelve));
